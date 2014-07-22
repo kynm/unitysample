031b23b678f0bbaf178194e0ef3d9f8bfb1f48e9
kynm <kynm@hus.edu.vn>
2014-07-22 09:49:30 +0700

    sample game unit 2d

---
+++ Assets/Script/BackGround.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackGround : MonoBehaviour {
+
+	public			float			speed;
+	public			int				numBGPanels = 6;
+
+	void Start(){
+	}
+
+	void Update(){
+		transform.Translate (Vector3.left * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.name == "BGLooper") {			
+			float widthOfBGObject = ((BoxCollider2D)collider2D).size.x - 0.01f;		
+
+			Vector3 pos = this.transform.position;			
+
+			pos.x += widthOfBGObject* numBGPanels;	
+
+			this.transform.position = pos;	
+		}
+	}
+
+}
